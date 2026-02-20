5ab02a86da7780adde4eaa470ae63c395919ba5f
Xtra3678 <tjcomer7@gmail.com>
2026-02-20 00:53:23 -0500

    Fansite First Push

---
+++ assets/static/load.js
@@ -0,0 +1,6 @@
+$(function(){
+  // Load local include fragments (relative paths) to avoid cross-origin issues
+  $("#navi").load("assets/static/navibar.html");
+  $("#sidebar").load("assets/static/sidebar.html");
+  $("#linkbox").load("assets/static/linkbox.html");
+});
